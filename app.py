26938dcb68f44dc2e27f5a7b1cc2122a535413bf
Florian Hofer <fh2412@users.noreply.huggingface.co>
2023-10-25 06:47:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ learn = load_learner('model.pkl')
 LABELS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def predict(img):
-    print(f"type: {img}")
+    print(f"type: {img[0]}")
     #testimg = PILImage.create(img)
     #print(f"type: {testimg}")
     pred, idx, probs = learn.predict(img[0])
-    #pred, idx, probs = learn.predict(testimg[0])
     print(f"pred: {learn.predict(img[0])}")
 
     #confidences = {LABELS[i]: v.item() for i, v in zip(pred, probs)}
