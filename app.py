d0dbdcefbb5a3f5bb8c268d66f1c1b1e86462f41
Florian Hofer <fh2412@users.noreply.huggingface.co>
2023-10-25 08:27:27 +0000

    stand gestern

--- app.py
+++ app.py
@@ -5,21 +5,13 @@ from fastai.vision.all import PILImage
 learn = load_learner('model.pkl')
 LABELS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-def get_img(data):
-    t1 = data.reshape(28,28)/255
-    t1 = np.stack([t1]*3,axis=0)
-    img = Image(FloatTensor(t1))
-    return img
-
-
 def predict(img):
     print(f"type: {img}")
-    testimg = PILImage.create(img)
-
-    imgtens = get_img(img)
-    print(f"type: {imgtens}")
-    pred, idx, probs = learn.predict(imgtens)
-    print(f"predictions: {learn.predict(imgtens)}")
+    #testimg = PILImage.create(img)
+    #print(f"type: {testimg}")
+    pred, idx, probs = learn.predict(img[0])
+    #pred, idx, probs = learn.predict(testimg[0])
+    print(f"pred: {learn.predict(img[0])}")
 
     #confidences = {LABELS[i]: v.item() for i, v in zip(pred, probs)}
     return dict(zip(LABELS, map(float,probs)))
