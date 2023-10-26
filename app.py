c117992b8375b292ed16bd28e46aca45070fa2ee
Florian Hofer <fh2412@users.noreply.huggingface.co>
2023-10-26 13:03:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from fastai.vision.all import *
 import gradio as gr
+from fastai.vision.all import PILImage
 
 learn = load_learner('model.pkl')
-
 categories = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-def classify_image(img):
-    pred,idx,probs = learn.predict(img)
-    print(f"Prediction: {pred}; Probability: {probs}")
+def predict(img):
+    pred, idx, probs = learn.predict(PILImage.create(img))
     return dict(zip(categories, map(float,probs)))
 
-image = gr.components.Image(shape=(28, 28))
-label = gr.components.Label()
-examples = ['zero.png', 'one.png']
+label = gr.outputs.Label()
+sp = gr.Sketchpad(shape=(28, 28),  image_mode="L")
 
-intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf = gr.Interface(fn=predict, inputs=sp, outputs=label)
 intf.launch(inline=False)
